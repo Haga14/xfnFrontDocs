db66c6fe776de9406bb7745ff2e00cd20fd3d2ad
ibra <ebrah210@gmail.com>
2020-12-12 18:19:19 -0600

    spelling

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:haga14/haga14.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:haga14/xfnFrontDocs.git main:gh-pages
+git push -f git@github.com:haga14/xfnFrontDocs.git master:gh-pages
 
 cd -
